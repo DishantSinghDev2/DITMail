2d82b4ee561ad02ed029c4f258a1c1b7f234776c
Dishant Singh <dishupandey57@gmail.com>
2025-06-22 14:02:03 +0530

    Add haraka dependency to package.json

--- smtp-pro/entrypoint.sh
+++ smtp-pro/entrypoint.sh
@@ -5,10 +5,10 @@ PORT=${REDIS_PORT:-6379}
 LOG=${LOG_LEVEL:-info}
 
 
-sed "/^host=/s/=.*/=${HOST}/" src/config/redis.ini > tmpfile && mv tmpfile src/config/redis.ini
-sed "/^port=/s/=.*/=${PORT}/" src/config/redis.ini > tmpfile && mv tmpfile src/config/redis.ini
+sed "/^host=/s/=.*/=${HOST}/" config/redis.ini > tmpfile && mv tmpfile config/redis.ini
+sed "/^port=/s/=.*/=${PORT}/" config/redis.ini > tmpfile && mv tmpfile config/redis.ini
 
-sed "/^level=/s/=.*/=${LOG}/" src/config/log.ini > tmpfile && mv tmpfile src/config/log.ini
+sed "/^level=/s/=.*/=${LOG}/" config/log.ini > tmpfile && mv tmpfile config/log.ini
 
 
 /usr/bin/supervisord
