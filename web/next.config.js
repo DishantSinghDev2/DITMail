0599bd63e79bf06d18facf3853448d5d5df7d004
Dishant Singh <dishisindia@gmail.com>
2025-06-27 21:25:58 +0530

    feat(sentry): integrate Sentry configuration and update Sentry packages to version 9.33.0

--- web/next.config.js
+++ web/next.config.js
@@ -21,4 +21,13 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, {
+  org: "dishis-technologies",
+  project: "javascript-nextjs",
+  // Only print logs for uploading source maps in CI
+  // Set to `true` to suppress logs
+  silent: !process.env.CI,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+});
