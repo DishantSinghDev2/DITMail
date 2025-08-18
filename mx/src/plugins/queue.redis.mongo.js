bab528f1cacfdfc4614ee3340666092e7e9595ad
Dishant Singh <dishantsinghdev@icloud.com>
2025-08-18 08:35:38 +0000

    added proper mongo uri in custom plugins

--- mx/src/plugins/queue.redis.mongo.js
+++ mx/src/plugins/queue.redis.mongo.js
@@ -3,7 +3,7 @@ const { simpleParser } = require('mailparser');
 const { MongoClient, ObjectId, GridFSBucket } = require('mongodb');
 
 let db, gfsBucket;
-MongoClient.connect(process.env.MONGO_URI, {
+MongoClient.connect(env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(client => {
