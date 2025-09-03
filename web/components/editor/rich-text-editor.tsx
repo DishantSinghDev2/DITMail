3e8c298b4fe80a01a1445b55315611187ff62ca5
Dishant Singh <dishisindia@gmail.com>
2025-09-03 14:17:01 +0000

    added proper type to all the text editor menu's buttons

--- web/components/editor/rich-text-editor.tsx
+++ web/components/editor/rich-text-editor.tsx
@@ -215,13 +215,13 @@ const RichTextEditor = forwardRef<RichTextEditorRef, RichTextEditorProps>(
             }`}
         >
           <div className="border-t p-2 bg-gray-50 flex flex-wrap items-center gap-1">
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("undo")} className="h-7 w-7 p-0"><Undo className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("redo")} className="h-7 w-7 p-0"><Redo className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("undo")} className="h-7 w-7 p-0"><Undo className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("redo")} className="h-7 w-7 p-0"><Redo className="h-3 w-3" /></Button>
             <Separator orientation="vertical" className="h-5 mx-1" />
 
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="sm" className="h-7 min-w-[80px] justify-between text-xs">{fontFamily}<Type className="h-3 w-3 ml-1" /></Button>
+                <Button type='button' variant="ghost" size="sm" className="h-7 min-w-[80px] justify-between text-xs">{fontFamily}<Type className="h-3 w-3 ml-1" /></Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent className="max-h-60 overflow-y-auto">
                 {FONT_FAMILIES.map((font) => <DropdownMenuItem key={font} onClick={() => changeFontFamily(font)} style={{ fontFamily: font }}>{font}</DropdownMenuItem>)}
@@ -230,7 +230,7 @@ const RichTextEditor = forwardRef<RichTextEditorRef, RichTextEditorProps>(
 
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="sm" className="h-7 w-10 justify-center text-xs">{fontSize}</Button>
+                <Button type='button' variant="ghost" size="sm" className="h-7 w-10 justify-center text-xs">{fontSize}</Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent>
                 {FONT_SIZES.map((size) => <DropdownMenuItem key={size} onClick={() => changeFontSize(size)}>{size}</DropdownMenuItem>)}
@@ -239,35 +239,35 @@ const RichTextEditor = forwardRef<RichTextEditorRef, RichTextEditorProps>(
 
             <Separator orientation="vertical" className="h-5 mx-1" />
 
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("bold")} className="h-7 w-7 p-0"><Bold className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("italic")} className="h-7 w-7 p-0"><Italic className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("underline")} className="h-7 w-7 p-0"><Underline className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("strikeThrough")} className="h-7 w-7 p-0"><Strikethrough className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("bold")} className="h-7 w-7 p-0"><Bold className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("italic")} className="h-7 w-7 p-0"><Italic className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("underline")} className="h-7 w-7 p-0"><Underline className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("strikeThrough")} className="h-7 w-7 p-0"><Strikethrough className="h-3 w-3" /></Button>
 
             <Popover>
-              <PopoverTrigger asChild><Button variant="ghost" size="sm" className="h-7 w-7 p-0"><Palette className="h-3 w-3" /></Button></PopoverTrigger>
-              <PopoverContent className="w-64 p-2"><div className="grid grid-cols-10 gap-1">{COLORS.map((color) => (<button key={color} className="w-5 h-5 rounded border hover:scale-110 transition-transform" style={{ backgroundColor: color }} onClick={() => executeCommand("foreColor", color)} />))}</div></PopoverContent>
+              <PopoverTrigger asChild><Button type='button' variant="ghost" size="sm" className="h-7 w-7 p-0"><Palette className="h-3 w-3" /></Button></PopoverTrigger>
+              <PopoverContent className="w-64 p-2"><div className="grid grid-cols-10 gap-1">{COLORS.map((color) => (<Button type='button' key={color} className="w-5 h-5 rounded border hover:scale-110 transition-transform" style={{ backgroundColor: color }} onClick={() => executeCommand("foreColor", color)} />))}</div></PopoverContent>
             </Popover>
             <Popover>
-              <PopoverTrigger asChild><Button variant="ghost" size="sm" className="h-7 w-7 p-0"><Highlighter className="h-3 w-3" /></Button></PopoverTrigger>
-              <PopoverContent className="w-64 p-2"><div className="grid grid-cols-10 gap-1">{COLORS.map((color) => (<button key={color} className="w-5 h-5 rounded border hover:scale-110 transition-transform" style={{ backgroundColor: color }} onClick={() => executeCommand("backColor", color)} />))}</div></PopoverContent>
+              <PopoverTrigger asChild><Button type='button' variant="ghost" size="sm" className="h-7 w-7 p-0"><Highlighter className="h-3 w-3" /></Button></PopoverTrigger>
+              <PopoverContent className="w-64 p-2"><div className="grid grid-cols-10 gap-1">{COLORS.map((color) => (<Button type='button' key={color} className="w-5 h-5 rounded border hover:scale-110 transition-transform" style={{ backgroundColor: color }} onClick={() => executeCommand("backColor", color)} />))}</div></PopoverContent>
             </Popover>
 
             <Separator orientation="vertical" className="h-5 mx-1" />
 
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("justifyLeft")} className="h-7 w-7 p-0"><AlignLeft className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("justifyCenter")} className="h-7 w-7 p-0"><AlignCenter className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("justifyRight")} className="h-7 w-7 p-0"><AlignRight className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("justifyLeft")} className="h-7 w-7 p-0"><AlignLeft className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("justifyCenter")} className="h-7 w-7 p-0"><AlignCenter className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("justifyRight")} className="h-7 w-7 p-0"><AlignRight className="h-3 w-3" /></Button>
 
             <Separator orientation="vertical" className="h-5 mx-1" />
 
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("insertUnorderedList")} className="h-7 w-7 p-0"><List className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("insertOrderedList")} className="h-7 w-7 p-0"><ListOrdered className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("outdent")} className="h-7 w-7 p-0"><Outdent className="h-3 w-3" /></Button>
-            <Button variant="ghost" size="sm" onClick={() => executeCommand("indent")} className="h-7 w-7 p-0"><Indent className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("insertUnorderedList")} className="h-7 w-7 p-0"><List className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("insertOrderedList")} className="h-7 w-7 p-0"><ListOrdered className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("outdent")} className="h-7 w-7 p-0"><Outdent className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={() => executeCommand("indent")} className="h-7 w-7 p-0"><Indent className="h-3 w-3" /></Button>
 
             <Separator orientation="vertical" className="h-5 mx-1" />
-            <Button variant="ghost" size="sm" onClick={insertLink} className="h-7 w-7 p-0"><LinkIcon className="h-3 w-3" /></Button>
+            <Button type='button' variant="ghost" size="sm" onClick={insertLink} className="h-7 w-7 p-0"><LinkIcon className="h-3 w-3" /></Button>
           </div>
         </div>
       </div>
